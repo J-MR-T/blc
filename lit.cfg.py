9f56ef3a4300d9e93c3bececa1e7614ba1709e55
Jim M. R. Teichgräber <jimmr.teichgraeber@tum.de>
2023-02-24 18:45:59 +0100

    ignore bSamples dir in tests

--- lit.cfg.py
+++ lit.cfg.py
@@ -6,6 +6,8 @@ config.test_format = lit.formats.ShTest(True)
 
 config.suffixes = ['.b']
 
+config.excludes = ['bSamples']
+
 config.test_source_root = os.path.dirname(__file__)
 config.test_exec_root = os.path.join(config.test_source_root, 'tests')
 

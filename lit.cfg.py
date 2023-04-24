00295c8d1834668dcef89a41b7b34145d9a72a94
Jim M. R. Teichgräber <jimmr.teichgraeber@tum.de>
2023-04-24 20:23:28 +0200

    lit: more prepared test execution substitutions

--- lit.cfg.py
+++ lit.cfg.py
@@ -15,7 +15,8 @@ config.test_exec_root = os.path.join(config.test_source_root, 'tests')
 
 config.substitutions.append(("%blc", os.path.join(config.test_source_root, "blc")))
 config.substitutions.append(("%FileCheckWithLLVMBackend" , "%blc -l %s %t && %t | FileCheck"))
+config.substitutions.append(("%FileCheckWithMLIRBackend" , "%blc -m %s %t && %t | FileCheck"))
 config.substitutions.append(("%FileCheckWithARMBackend", r"%blc -a %s | aarch64-linux-gnu-gcc -g -x assembler -o %t - && qemu-aarch64 -L /usr/aarch64-linux-gnu %t | FileCheck"))
-config.substitutions.append(("%compareBothBackends", r"%blc -l %s %t-1; %blc -a %s | aarch64-linux-gnu-gcc -g -x assembler -o %t - && qemu-aarch64 -L /usr/aarch64-linux-gnu %t | diff <(%t-1) -"))
+config.substitutions.append(("%compareBothBackends", r"%blc -l %s %t-llvm; %blc -m %s %t-mlir; %blc -a %s | aarch64-linux-gnu-gcc -g -x assembler -o %t-arm - && diff <(%t-llvm) <(qemu-aarch64 -L /usr/aarch64-linux-gnu %t-arm)"))
 
 config.recursiveExpansionLimit = 3
